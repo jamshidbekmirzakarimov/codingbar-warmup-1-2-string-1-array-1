99e8c97526a3995459d833e8a4b54ea6defbce9e
Jamshidbek <jamshidbekmirzakarimov6@gmail.com>
2023-01-31 08:43:31 +0500

    warmup-1 finished

--- js/warmup-2.js
+++ js/warmup-2.js
@@ -1,6 +1,6 @@
 //! Warmup-2
 
-//* 1-masala
+//* 1-masala stringTimes
 // let str = prompt("string kiriting");
 
 // let n = prompt("n ta son kiriting");
@@ -11,7 +11,7 @@
 // }
 // console.log(result);
 
-//* 2-masala
+//* 2-masala frontTimes
 
 // let str = "jamshidbek";
 // let n = 3;
@@ -48,4 +48,94 @@
 // }
 // console.log(res);
 
-//*3-masala
+//*3-masala countXX
+
+// function countXX(str){
+//   let count = 0;
+//   for (let i = 0; i < str.length; i++) {
+//     if (str[i] === "x" && str[i + 1] === "x") {
+//       count++;
+//     }
+//   }
+//   console.log(count);
+// }
+// countXX("abcxxx")
+
+//*4-masala
+
+// function doubleX(str){
+//   let a = str.indexOf("x");
+//   if (a < 1) {
+//     console.log(false);
+//   }
+// }
+// doubleX("aafxxasd")
+
+//*arrayCount9
+
+// function arrayCount9(nums){
+//   let count = 0;
+//   for (let i = 0; i < nums.length; i++){
+//     if (nums[i] == 9) {
+//       count++;
+//     }
+//   }
+//   console.log(count);
+// }
+
+// arrayCount9([1,4,9,5,9,3,9])
+
+//*arrayFront9
+
+// function arrayFront9(nums) {
+//   let a = nums.length;
+//   if (a > 4) {
+//     a = 4;
+//   }
+//   for (let i = 0; i < a; i++){
+//     if (nums[i] == 9) {
+//       return false;
+//     }
+//   }
+//   console.log(nums);
+// }
+// arrayFront9(nums)
+
+//*array123
+
+// function array123(nums) {
+//   for (let i = 0; i < nums.length - 2; i++) {
+//       if (nums[i] === 1 && nums[i + 1] === 2 && nums[i + 2] === 3) {
+//           return true;
+//       }
+//   }
+//   return false;
+// }
+// array123[1,2,3,9,7,3]
+
+
+//* stringX
+
+// function stringX(str) {
+//   let newstr = "";
+
+//   for (let i = 0; i < str.length; i++){
+//     if (i === 0 || i === str.length-1) {
+//       newstr += str[i];
+//     } else if (str[i] !== "x") {
+//       newstr += str[i];
+//     }
+//   }
+//   console.log(newstr);
+// }
+// stringX("xxhicxdcdxxx");
+
+//*
+
+
+
+
+
+
+
+
